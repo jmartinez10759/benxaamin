5e4c646c79e53261cc5787cddc221ac4d60c3ef4
jmartinez10759 <jorge.martinez.developer@gmail.com>
2018-02-16 11:28:50 -0600

    Inicializacion de Proyecto

---
+++ app/Http/Controllers/api/EmpleadoApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EmpleadoApiController extends Controller
+{
+    
+	public function index(){
+		echo "llego aqui con un demonio, aqui creare mi crud de endpoint";
+	}
+
+}
